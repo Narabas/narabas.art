ceb17116a5751a376560989abad021d5e041f750
Narabas <49916720+Narabas@users.noreply.github.com>
2023-07-18 09:40:04 -0400

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -45,3 +45,9 @@ function toggleNav() {
   }
 
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+      document.querySelector("body").classList.add("loaded");
+  }, 10)
+});
